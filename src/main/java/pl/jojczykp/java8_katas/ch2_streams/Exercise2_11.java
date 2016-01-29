d38e11b35e705fa4a77fc12eb6763fb21f4a847a
mrJojo <pawel.jojczyk@gmail.com>
2016-01-29 20:28:36 +0100

    Streams - Exercise 11 - Parallel array update

---
+++ src/main/java/pl/jojczykp/java8_katas/ch2_streams/Exercise2_11.java
@@ -0,0 +1,16 @@
+package pl.jojczykp.java8_katas.ch2_streams;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+public final class Exercise2_11 {
+
+	public void shouldPopulateConcurrently(Stream<String> stream, String[] destination) {
+		AtomicInteger idx = new AtomicInteger(0);
+
+		stream
+				.parallel().unordered()
+				.forEach(s -> destination[idx.getAndIncrement()] = s);
+	}
+
+}
---
+++ src/test/java/pl/jojczykp/java8_katas/ch2_streams/Exercise2_11Test.java
@@ -0,0 +1,25 @@
+package pl.jojczykp.java8_katas.ch2_streams;
+
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+public class Exercise2_11Test {
+
+	private Exercise2_11 testee = new Exercise2_11();
+
+	@Test
+	public void shouldPopulateSingleArrayListConcurrently() {
+		String[] source = {"a", "b", "c", "d", "e"};
+		String[] destination = new String[source.length];
+
+		testee.shouldPopulateConcurrently(Stream.of(source), destination);
+
+		assertThat(asList(destination), containsInAnyOrder(source));
+	}
+
+}
