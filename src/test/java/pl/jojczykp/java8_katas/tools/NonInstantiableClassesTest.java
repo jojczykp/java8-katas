1b8b82eefb7d5193387b1bcf30e5453f1a345e64
mrJojo <pawel.jojczyk@gmail.com>
2016-01-29 02:18:54 +0100

    Refactoring - Rename classes

--- src/test/java/pl/jojczykp/java8_katas/tools/NonInstantiableClassesTest.java
+++ src/test/java/pl/jojczykp/java8_katas/tools/NonInstantiableClassesTest.java
@@ -1,9 +1,9 @@
 package pl.jojczykp.java8_katas.tools;
 
 import org.junit.Test;
-import pl.jojczykp.java8_katas.ch1_lambdas.Exercise6;
-import pl.jojczykp.java8_katas.ch1_lambdas.Exercise7;
-import pl.jojczykp.java8_katas.ch1_lambdas.Exercise9;
+import pl.jojczykp.java8_katas.ch1_lambdas.Exercise1_6;
+import pl.jojczykp.java8_katas.ch1_lambdas.Exercise1_7;
+import pl.jojczykp.java8_katas.ch1_lambdas.Exercise1_9;
 
 import java.lang.reflect.Constructor;
 
@@ -15,9 +15,9 @@ public class NonInstantiableClassesTest {
 
 	@Test
 	public void shouldHavePrivateSoleConstructor() throws Exception {
-		shouldHavePrivateSoleConstructor(Exercise6.class);
-		shouldHavePrivateSoleConstructor(Exercise7.class);
-		shouldHavePrivateSoleConstructor(Exercise9.class);
+		shouldHavePrivateSoleConstructor(Exercise1_6.class);
+		shouldHavePrivateSoleConstructor(Exercise1_7.class);
+		shouldHavePrivateSoleConstructor(Exercise1_9.class);
 	}
 
 	private <T> void shouldHavePrivateSoleConstructor(Class<T> clazz) throws Exception {
