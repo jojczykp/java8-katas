d359002a5f8be8b43d69644955a2a1838f32cb06
mrJojo <pawel.jojczyk@gmail.com>
2016-02-01 20:03:10 +0100

    Lambda Programming - Exercise 3 - Assert

---
+++ src/main/java/pl/jojczykp/java8_katas/ch3_lambda_programming/Exercise_3_3_Assert.java
@@ -0,0 +1,13 @@
+package pl.jojczykp.java8_katas.ch3_lambda_programming;
+
+import java.util.function.BooleanSupplier;
+
+public class Exercise_3_3_Assert {
+
+	public void assert8(BooleanSupplier condition) {
+		if (!condition.getAsBoolean()) {
+			throw new AssertionError();
+		}
+	}
+
+}
