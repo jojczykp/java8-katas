467e77fc2b4fbe48b595d5579787dbc637e6e807
mrJojo <pawel.jojczyk@gmail.com>
2016-02-02 23:57:00 +0100

    Lambda Programming - Exercise 7 - Comparators Composition

---
+++ src/main/java/pl/jojczykp/java8_katas/ch3_lambda_programming/Exercise_3_7_ComparatorsComposition.java
@@ -0,0 +1,28 @@
+package pl.jojczykp.java8_katas.ch3_lambda_programming;
+
+import java.util.Comparator;
+import java.util.function.Function;
+
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.naturalOrder;
+import static java.util.Comparator.reverseOrder;
+
+public class Exercise_3_7_ComparatorsComposition {
+
+	public Comparator<String> getComposedStringComparator(
+			boolean reversed, boolean caseInsensitive, boolean spaceInsensitive) {
+		Function<String, String> withModification = Function.identity();
+
+		if (caseInsensitive) {
+			withModification = withModification.andThen(String::toUpperCase);
+		}
+
+		if (spaceInsensitive) {
+			withModification = withModification.andThen(s -> s.replaceAll(" ", ""));
+		}
+
+		return comparing(withModification, reversed ? reverseOrder() : naturalOrder());
+
+	}
+
+}
