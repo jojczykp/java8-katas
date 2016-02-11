b0b4549fedf94bcf1e40a42bb304d602e6e43378
mrJojo <pawel.jojczyk@gmail.com>
2016-02-11 17:09:59 +0100

    Refactoring - static import

--- src/test/java/pl/jojczykp/java8_katas/ch5_date_time/Exercise_5_12_TimeZoneAgnosticAlertsTest.java
+++ src/test/java/pl/jojczykp/java8_katas/ch5_date_time/Exercise_5_12_TimeZoneAgnosticAlertsTest.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.time.ZonedDateTime.parse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 
@@ -23,10 +24,10 @@ public class Exercise_5_12_TimeZoneAgnosticAlertsTest {
 		final LocalDateTime localDateTime = LocalDateTime.of(2016, 2, 11, 16, 5);
 		final Duration duration = Duration.ofHours(1);
 		final Map<ZonedDateTime, String> messages = new HashMap<>();
-		messages.put(ZonedDateTime.parse("2016-02-11T16:09:30+01:00[Europe/Warsaw]"), "Event in Warsaw");
-		messages.put(ZonedDateTime.parse("2016-02-11T16:09:30+10:00[Australia/Sydney]"), "Event in Sydney");
-		messages.put(ZonedDateTime.parse("2016-02-11T07:45:00-08:00[America/Los_Angeles]"), "Event in Los Angeles");
-		messages.put(ZonedDateTime.parse("2016-02-11T17:10:11+01:00[Europe/Madrid]"), "Event in Madrid");
+		messages.put(parse("2016-02-11T16:09:30+01:00[Europe/Warsaw]"), "Event in Warsaw");
+		messages.put(parse("2016-02-11T16:09:30+10:00[Australia/Sydney]"), "Event in Sydney");
+		messages.put(parse("2016-02-11T07:45:00-08:00[America/Los_Angeles]"), "Event in Los Angeles");
+		messages.put(parse("2016-02-11T17:10:11+01:00[Europe/Madrid]"), "Event in Madrid");
 
 		List<String> result = testee.getIncomingEvents(messages, localZone, localDateTime, duration);
 
